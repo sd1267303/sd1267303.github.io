a81ee7ba3bd3c08f2167f6cb233c9d7315b7f241
sd1267303 <30584464+sd1267303@users.noreply.github.com>
2020-12-16 14:34:39 +0800

    1
    2

--- device12_files/assets.js
+++ device12_files/assets.js
@@ -1,1 +1,1 @@
-var SPLINE_ASSETS_LIST = { fonts: ['roboto_regular','roboto_black_italic'], images: [], models: [], animations: [] }
+var SPLINE_ASSETS_LIST = { fonts: ['Microsoft YaHei','Microsoft YaHei'], images: [], models: [], animations: [] }
